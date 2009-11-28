4b4a5e93dfd99286fd1a81a5c79490fcc41cfb52
mudtail <maff130@gmail.com>
2009-11-28 21:31:20 +0000

    Fixed bulk-download facility. It now uses a class found on Devco.net for compression, as opposed to the built-in ZipArchive class in PHP.

--- slurp/zipfiles.php
+++ slurp/zipfiles.php
@@ -18,21 +18,21 @@ if(!isset($_SESSION['zFiles']) || count($_SESSION['zFiles']) == 0) {
 	die();
 }
 $fn = "stored/".time().".zip";
-$zip = new ZipArchive;	
-$zh = $zip->open($fn, ZipArchive::CREATE);
-if($zh === true) {
-	foreach($_SESSION['zFiles'] as $short) {
-		$q = $db->query("SELECT * FROM ".TB_MAIN." WHERE short='$short' AND isURL = 2");
-		if($q->num_rows > 0) {
-			$q = $q->fetch_assoc();
-			$zip->addFromString($q['filename'], file_get_contents($q['notshort']));
-		}
+include('zipfile.inc.php');
+$zip = new zipfile();
+foreach($_SESSION['zFiles'] as $short) {
+	$q = $db->query("SELECT * FROM ".TB_MAIN." WHERE short='$short' AND isURL = 2");
+	if($q->num_rows > 0) {
+		$q = $q->fetch_assoc();
+		$zip->add_file(file_get_contents($q['notshort']), $q['filename']);
 	}
-	$zip->close();
-	header('Content-Length: '.filesize($fn));
-	header("Content-Disposition: filename=$fn");
-	header('Content-Type: application/octet-stream');
-	readfile($fn);
-	unlink($fn);
 }
+$fh = fopen($fn, 'w');
+fwrite($fh, $zip->file());
+fclose($fh);
+header('Content-Length: '.filesize($fn));
+header("Content-Disposition: filename=$fn");
+header('Content-Type: application/octet-stream');
+readfile($fn);
+unlink($fn);
 ?>
