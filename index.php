9d29f12bbbc2bc979841c66a96132a7161d680b6
MaffC <maff@maff.im>
2011-05-02 03:02:57 +0100

    Adding the landing page, which more or less initialises the whole system.

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require_once('./slurp/index.php'); ?>
+<!--DOCTYPE-->
+<html>
+	<head>
+		<title>Slurp! 2.0 - Monster</title>
+		<link rel="stylesheet" href="/style.css" type="text/css" /> <!--The only thing that's being kept from 1.0 branch is the stylesheet-->
+		<!--Meta shit goes here-->
+	</head>
+	<body>
+		<h1>Well hey there.</h1>
+		<h2>You've happened upon Slurp!'s landing page. If you don't know what Slurp! is, then you should <a href="http://github.com/maffc/slurp">click here</a>.</h2>
+	</body>
+</html>
